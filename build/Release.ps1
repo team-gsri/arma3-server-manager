c42893333b79533e984f5351066d3dd2ba163d0d
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-04-30 19:01:42 +0200

    fix: cleanup and restore path filter

--- build/Release.ps1
+++ build/Release.ps1
@@ -17,7 +17,6 @@ $Destination = Join-Path $Root out/ArmaServer
 Copy-Item $Sources/.templates $Destination/.templates -Recurse -Force
 Copy-Item $Builds/ArmaServer.psd1 $Destination/ArmaServer.psd1 -Force
 Get-ChildItem $Sources -Filter '*.ps1' | & $Builds/New-PowershellModuleFile.ps1 -DestinationPath $Destination/ArmaServer.psm1 -Force
-Import-PowerShellDataFile $Destination/ArmaServer.psd1 | ConvertTo-Json | Write-Verbose
-Get-ChildItem -Recurse $Destination
 Update-ModuleManifest -ModuleVersion $Version $Destination/ArmaServer.psd1
+Import-PowerShellDataFile $Destination/ArmaServer.psd1 | ConvertTo-Json | Write-Verbose
 Publish-Module -Path $Destination -NuGetApiKey $ApiKey
