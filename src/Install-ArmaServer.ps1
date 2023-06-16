2179eb5ecdde15511bf7d8ab4d749a10b0df6c2d
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-06-16 19:21:08 +0200

    fix: steamcmd cannot change directory when connected (#27)
    * fix: steamcmd cannot change directory when connected
    
    * ci: fix main branch name

--- src/Install-ArmaServer.ps1
+++ src/Install-ArmaServer.ps1
@@ -15,6 +15,8 @@ Begin {
 
 End {
   Stop-ArmaServer -ConfigFilename $ConfigFilename
+  New-Item $Config.MasterPath -ItemType Directory -Force | Out-Null
+  New-Item $Config.WorkshopPath -ItemType Directory -Force | Out-Null
   $Addons | ArmaServer-InvokeDownload -MasterPath $Config.MasterPath -WorkshopPath $Config.WorkshopPath -Quit
   $Addons | ArmaServer-InstallBohemiaKeys -DestinationPath $KeysPath -WorkshopPath $Config.WorkshopPath
   $Config.Missions | ArmaServer-InstallMission -DestinationPath $MissionsPath
