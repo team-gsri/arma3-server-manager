1b91f81511378d42c6f46bc3bfc8cfc1e1d07f7f
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-12-06 03:18:17 +0100

    feat: disable update on start

--- src/Start-ArmaServer.ps1
+++ src/Start-ArmaServer.ps1
@@ -6,21 +6,9 @@ param (
   $ConfigFilename
 )
 
-Begin {
-  $Config = Import-PowerShellDataFile $ConfigFilename
-  $TranscriptPath = Join-Path $Config.ProfilePath pslogs
-  Start-Transcript -OutputDirectory $TranscriptPath
-}
-
-Process {
-  Stop-ArmaServer -ConfigFilename $ConfigFilename
-  Install-ArmaServer -ConfigFilename $ConfigFilename
-  ArmaServer-InvokeServerProcess -ConfigFilename $ConfigFilename
-  if ($Config.Headless) {
-    ArmaServer-InvokeHeadlessProcess -ConfigFilename $ConfigFilename
-  }
-}
-
-End {
-  Stop-Transcript
+$Config = Import-PowerShellDataFile $ConfigFilename
+Stop-ArmaServer -ConfigFilename $ConfigFilename
+ArmaServer-InvokeServerProcess -ConfigFilename $ConfigFilename
+if ($Config.Headless) {
+  ArmaServer-InvokeHeadlessProcess -ConfigFilename $ConfigFilename
 }
