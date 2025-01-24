fe9354ee2e249bb42a51c998a2803d29d9d1c906
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2025-01-24 20:45:23 +0100

    refactor: move sources

---
+++ src/ArmaServer/ArmaServer.psm1
@@ -0,0 +1,4 @@
+function Install-ArmaServer { & $PSScriptRoot/Install-ArmaServer.ps1 }
+function Register-ArmaServerTask { & $PSScriptRoot/Register-ArmaServerTask.ps1 }
+function Start-ArmaServer { & $PSScriptRoot/Start-ArmaServer.ps1 }
+function Stop-ArmaServer { & $PSScriptRoot/Stop-ArmaServer.ps1 }
